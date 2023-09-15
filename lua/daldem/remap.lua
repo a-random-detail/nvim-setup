79f9dec390a2905f24a8aa312c4b61ad6f9ac09c
Dalton DeMaria <dalton.demaria@gmail.com>
2023-09-15 15:57:49 -0500

    add clipboard integration

--- lua/daldem/remap.lua
+++ lua/daldem/remap.lua
@@ -1,3 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.keymap.set("v", "<C-x>", ":!pbcopy<CR>")
+vim.keymap.set("v", "<C-c>", ":w !pbcopy<CR><CR>")
+vim.keymap.set("n", "<C-v>", ":r !pbpaste<CR><CR>")
+vim.keymap.set("i", "<C-v>", "<Esc>:r !pbpaste<CR><CR>")
